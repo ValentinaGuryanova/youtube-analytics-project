f7b5db478d0e06f7755ba1d8f6a58ef65d1814bd
ValentinaGuryanova <gvalushka@mail.ru>
2023-08-08 15:47:07 +0300

    Добавлены и инициализированы классы Video и PLVideo

--- src/video.py
+++ src/video.py
@@ -1,47 +1,37 @@
-from src.channel import youtube
-import isodate
+
+import os
+from googleapiclient.discovery import build
 
 class Video:
-    def __init__(self, video_id: str) -> None:
 
-        self.video_id = video_id
+    API_KEY: str = os.getenv('API_KEY')
+    YOUTYBE = build('youtube', 'v3', developerKey=API_KEY)
+    def __init__(self, video_id: str):
 
-        video_id = 'AWX4JnAnjBE'
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id).execute()
+        video = Video.YOUTYBE.videos().list(part='snippet,statistics', id=video_id).execute()
 
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+        try:
 
+            self.video_id = video_id
+            self.title = video["items"][0]["snippet"]["title"]
+            self.url = "https://youtube/" + video_id
+            self.number_of_views = video["items"][0]["statistics"]["viewCount"]
+            self.like_count = video["items"][0]["statistics"]["likeCount"]
 
-class PLVideo:
-    def __init__(self, video_id, playlist_id):
-        self.video_id = video_id
-        video_id = '4fObz_qw9u4'
+        except IndexError:
+
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.number_of_views = None
+            self.like_count = None
+
+class PLVideo(Video):
+    def __init__(self, video_id:str, playlist_id:str):
+        super().__init__(video_id)
         self.playlist_id = playlist_id
-        playlists = youtube.playlists().list(id=video_id,
-                                             part='contentDetails,snippet',
-                                             maxResults=50,
-                                             ).execute()
-        for playlist in playlists['items']:
-            print(playlist)
-            print()
-
-        playlist_id = 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC'
-        playlist_videos = youtube.playlists().list(part="snippet", id=playlist_id, maxResults=50).execute()
-        video_ids = [video['id'] for video in playlist_videos['items']]
-        video_response = youtube.videos().list(part='contentDetails,statistics',
-                                               id=','.join(video_ids)
-                                               ).execute()
-
-        for video in video_response['items']:
-            # YouTube video duration is in ISO 8601 format
-            iso_8601_duration = video['contentDetails']['duration']
-            duration = isodate.parse_duration(iso_8601_duration)
-            print(duration)
 
     def __str__(self):
-        return f'{self.video_id}, {self.playlist_id}'
+        return self.title
 
 
